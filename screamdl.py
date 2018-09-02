d13d5f0a6c20d7db7dabf142fd96bf0059444295
Mary <ipadlover8332@gmail.com>
2018-09-02 17:02:11 +0000

    Add checks for supported sites

--- screamdl.py
+++ screamdl.py
@@ -4,12 +4,32 @@ import youtube_dl
 import tornado.ioloop
 import tornado.web
 import urllib.error
+import math
+import re
 
 ydl = youtube_dl.YoutubeDL({})
+
+all_extractors = youtube_dl.list_extractors(math.inf)
+supported = []
+supported_re = []
+bad_extractors = ["generic"]
+
+for extractor in all_extractors:
+    if extractor.IE_NAME in bad_extractors:
+        continue
+    if not isinstance(extractor, youtube_dl.extractor.common.SearchInfoExtractor):
+        supported.append(extractor.IE_NAME)
+        supported_re.append(extractor._VALID_URL)
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         video_url = self.get_query_argument("v", default="")
+        if not any([re.search(extractor_re, video_url) for extractor_re in supported_re]):
+            self.set_status(415)
+            return self.write({
+                "message": "UNSUPPORTED_SITE",
+                "supported": supported
+            })
         with ydl:
             try:
                 info = ydl.extract_info(video_url, download=False)
